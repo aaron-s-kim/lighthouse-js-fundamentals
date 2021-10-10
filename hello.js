f126e668f88ff38ad0efd10430f317904f701604
Aaron K <tonberryspawn@gmail.com>
2021-10-09 20:41:03 -0700

    hello message examples

--- hello.js
+++ hello.js
@@ -1,7 +1,17 @@
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
-
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
