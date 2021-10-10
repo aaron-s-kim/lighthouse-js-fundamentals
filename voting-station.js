c4db37078c74ec371062a3595002ae5ef82b4c27
Aaron K <tonberryspawn@gmail.com>
2021-10-09 21:23:48 -0700

    voting station selection

---
+++ voting-station.js
@@ -0,0 +1,24 @@
+// Voting Station
+// take array of possible voting stations, and return names of appropriate stations
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+const chooseStations = function (stations) {
+  let goodStations = [];
+  for (var station of stations) {
+    if (station[1] >= 20) {
+      if (station[2] === "school" || station[2] === "community centre") {
+        goodStations.push(station[0]);
+      }
+    }
+  }
+  return goodStations;
+}
+
+console.log(chooseStations(stations));
+
+
