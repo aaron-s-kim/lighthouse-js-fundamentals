c016ea06e98ae894dfb9562a7629c642be864e89
Aaron K <tonberryspawn@gmail.com>
2021-10-11 21:52:47 -0700

    looping numbers

---
+++ looping-range.js
@@ -0,0 +1,17 @@
+function range (start, end, step) {
+  const array = [];
+  if (start === undefined || end === undefined || step === undefined) {
+    return [];
+  } else if (start > end || step <= 0) {
+    return [];
+  } else {
+    for (var i = start; i <= end; i += step) {
+      array.push(i);
+    }
+    return array;
+  }
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
